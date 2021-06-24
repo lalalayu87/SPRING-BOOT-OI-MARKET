8f151ab0298c3b3d4333b20300be3ef54dc5affe
lalalayu87 <lalalayu87@gmail.com>
2021-06-24 17:54:29 +0900

    SPRING BOOT 오이마켓 첫번째 커밋

---
+++ oimarket/src/main/java/com/oimarket/OimarketApplication.java
@@ -0,0 +1,13 @@
+package com.oimarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OimarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OimarketApplication.class, args);
+	}
+
+}
---
+++ oimarket/src/test/java/com/oimarket/OimarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oimarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OimarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
